f15a9279e17d9fa7a8a1b9d42b489c11533ae91d
Marshall Offutt <marshalloffutt@gmail.com>
2018-09-06 20:37:54 -0500

    Adds setup files

---
+++ main.js
@@ -0,0 +1,41 @@
+let lang = {
+    happy: ["щастлив", "hamingjusöm", "ευτυχισμένος"],
+    halloween: ["Хелоуин", "Hrekkjavaka", "Απόκριες"],
+    ghost: ["призрак", "draugur", "φάντασμα"],
+    witch: ["вещица", "norn", "μάγισσα"],
+    spooky: ["зловещ", "skuggalegt", "στοιχειωμένος"],
+    haunting: ["натрапчива", "ásækja", "στέκι"],
+    fright: ["уплаха", "ótti", "τρομάρα"]
+}
+
+
+//This way seems too long. I will keep it for now in case I want to return to it, but I will be using the cleaner one.
+// let bulgarian = {
+//     happy: "щастлив", 
+//     halloween: "Хелоуин",
+//     ghost: "призрак",
+//     witch: "вещица",
+//     spooky: "зловещ",
+//     haunting: "натрапчива",
+//     fright: "уплаха"
+// }
+
+// let icelandic = {
+//     happy: "hamingjusöm", 
+//     halloween: "Hrekkjavaka",
+//     ghost: "draugur",
+//     witch: "norn",
+//     spooky: "skuggalegt",
+//     haunting: "ásækja",
+//     fright: "ótti"
+// }
+
+// let greek = {
+//     happy: "ευτυχισμένος", 
+//     halloween: "Απόκριες",
+//     ghost: "φάντασμα",
+//     witch: "μάγισσα",
+//     spooky: "στοιχειωμένος",
+//     haunting: "στέκι",
+//     fright: "τρομάρα"
+// }
